85ed2087f04e55fe473f25e64832368cd73d0e4c
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2022-03-05 21:57:19 +0300

    Add main class and empty configuration

---
+++ src/main/java/digital/zelenev/product_task/configuration/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package digital.zelenev.product_task.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import java.util.Random;
+
+@Configuration
+public class ApplicationConfiguration {
+
+}
