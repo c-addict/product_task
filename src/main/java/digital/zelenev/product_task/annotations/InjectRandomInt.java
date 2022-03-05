0b57a6d7542311a8efdcd50bcdcc76bcbd570ee3
Zelenev Nikolay <zelenev.kolya.177@gmail.com>
2022-03-05 21:56:14 +0300

    Add InjectRandomIntAnnotation and Bean post processor for it

---
+++ src/main/java/digital/zelenev/product_task/annotations/InjectRandomInt.java
@@ -0,0 +1,12 @@
+package digital.zelenev.product_task.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectRandomInt {
+
+    int min();
+    int max();
+
+}
